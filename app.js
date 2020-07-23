de5863ee311507743cfacb0b818e148047753803
chandhana <chan.keen3112000@gmail.com>
2020-07-23 18:51:29 +0000

    remove unwanted file

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const http = require("http");
 var express = require("express");
 var app = express();
-var bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
 app.use(express.static(__dirname+"/public"));
