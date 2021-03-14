bc48267cc63c15e05e1915961a634ce41f5ed269
chandhana <chan.keen3112000@gmail.com>
2021-03-14 05:24:13 +0000

    socket.io added and connection established

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const server =require("http").Server(app);
+const io =require("socket.io")(server);
+users=[];
+connections=[];
 
 //extension .ejs 
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
 app.use(express.static(__dirname+"/jscript"));
+app.use(express.static("/workspace/MicrosoftEngage2020/tictactoe/client.js"));
 
 //request homePage
 app.get("/", function(req, res){
@@ -21,9 +26,19 @@ app.get("*", function(req, res){
 	res.send("<h1>SORRY! PAGE NOT FOUND!!</h1>");
 });
 
+
+io.sockets.on("connection",function(socket){
+	connections.push(socket);
+	console.log("Connected:  %s sockets connected",connections.length);
+	
+	//disconnect
+	connections.splice(connections.indexOf(socket),1);
+	console.log("Disconnected: %s  sockets connected",connections.length);
+});
+
 //server started at port 3000
 var port = process.env.PORT || 3000;
-app.listen(port,function(){
+server.listen(port,function(){
 	console.log("tictactoe Server started! at 3000");
 });
 
